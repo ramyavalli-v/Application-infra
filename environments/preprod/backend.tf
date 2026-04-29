1f84d8d651a6718ad30f4ec6066c208926718db5
ramyavalli-v <ramyavalli2509@gmail.com>
2026-04-29 11:36:12 +0530

    app deployment

--- environments/preprod/backend.tf
+++ environments/preprod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "c3ops-terraform-state1"
+    bucket = "c3ops-terraform-state-133630512742-ap-south-1-an"
     key    = "app-infra/preprod/terraform.tfstate"
     region = "ap-south-1"
   }
--- environments/preprod/data.tf
+++ environments/preprod/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "core" {
   backend = "s3"
   config = {
-    bucket = "c3ops-terraform-state1"
+    bucket = "c3ops-terraform-state-133630512742-ap-south-1-an"
     key    = "core-infra/ap-south-1/terraform.tfstate"
     region = var.region
   }
